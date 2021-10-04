938eb067080e41088dee4ee9cbbe70d325abd034
Jack Francis <jackfrancis@gmail.com>
2021-10-04 12:15:54 -0700

    update examples with correct apt upgrade command (#2)

--- examples/update-containerd.sh
+++ examples/update-containerd.sh
@@ -2,5 +2,5 @@
 # assumes run as root
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin
 DEBIAN_FRONTEND=noninteractive
-apt-get upgrade moby-containerd -y
+apt-get install --only-upgrade moby-containerd -y
 systemctl restart kubelet
